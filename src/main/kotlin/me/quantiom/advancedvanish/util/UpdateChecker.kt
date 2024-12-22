ce0e2259b683357bb6e85fc65bbc1bcf9cbc0a99
Daisy <amari@amari.wtf>
2024-12-22 01:42:05 +0000

    Fix update checker

--- src/main/kotlin/me/quantiom/advancedvanish/util/UpdateChecker.kt
+++ src/main/kotlin/me/quantiom/advancedvanish/util/UpdateChecker.kt
@@ -12,14 +12,14 @@ import java.util.function.Consumer
 object UpdateChecker {
     private const val REPO_OWNER = "fu3i0n"
     private const val REPO_NAME = "AdvancedVanish"
-    private const val RELEASE_TAG = "1.2.8"
 
     fun getVersion(consumer: Consumer<String?>) {
         Bukkit.getScheduler().runTaskAsynchronously(
             AdvancedVanish.instance!!,
             Runnable {
                 try {
-                    val url = URL("https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/tags/$RELEASE_TAG")
+                    // Fetch the latest release dynamically
+                    val url = URL("https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest")
                     val connection = url.openConnection() as HttpURLConnection
                     connection.requestMethod = "GET"
                     connection.connect()
@@ -32,6 +32,7 @@ object UpdateChecker {
                                 val json = JSONObject(response)
                                 val latestVersion = json.getString("tag_name")
                                 consumer.accept(latestVersion)
+                                checkForUpdate(latestVersion)
                             }
                         }
                     } else {
@@ -42,5 +43,31 @@ object UpdateChecker {
                 }
             },
         )
+    }
+
+    private fun checkForUpdate(latestVersion: String) {
+        val currentVersion = AdvancedVanish.instance?.description?.version ?: return
+        if (isUpdateAvailable(currentVersion, latestVersion)) {
+            AdvancedVanish.instance!!.logger.info("Update available! Current version: $currentVersion, Latest version: $latestVersion")
+        } else {
+            AdvancedVanish.instance!!.logger.info("You are running the latest version ($currentVersion).")
+        }
+    }
+
+    private fun isUpdateAvailable(currentVersion: String, latestVersion: String): Boolean {
+        return compareVersions(currentVersion, latestVersion) < 0
+    }
+
+    private fun compareVersions(version1: String, version2: String): Int {
+        val versionParts1 = version1.split(".").map { it.toInt() }
+        val versionParts2 = version2.split(".").map { it.toInt() }
+
+        val maxLength = maxOf(versionParts1.size, versionParts2.size)
+        for (i in 0 until maxLength) {
+            val part1 = versionParts1.getOrElse(i) { 0 }
+            val part2 = versionParts2.getOrElse(i) { 0 }
+            if (part1 != part2) return part1 - part2
+        }
+        return 0
     }
 }
